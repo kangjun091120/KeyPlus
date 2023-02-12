bf5beffe977905edd31fd938fe4c0a6a6943a970
kangjun1120 <kangjun1120@126.com>
2023-02-12 22:00:59 +0800

    upgrade

--- main.py
+++ main.py
@@ -2,13 +2,8 @@ from pynput import mouse, keyboard
 import monitor as on
 
 
-listener = mouse.Listener(
-    on_move=on.move,
-    on_click=on.click,
-    on_scroll=on.scroll)
+listener = mouse.Listener(on_move=on.move, on_click=on.click, on_scroll=on.scroll)
 listener.start()
 
-with keyboard.Listener(
-        on_press=on.press,
-        on_release=on.release) as listener:
+with keyboard.Listener(on_press=on.press, on_release=on.release) as listener:
     listener.join()
