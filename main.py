a2280abb6c3d0c6a41d21f4ebd2d18797430f0d8
kangjun1120 <kangjun1120@126.com>
2023-02-13 10:14:08 +0800

    upgrade

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from pynput import mouse, keyboard
 import monitor as on
+import webview
 
+window = webview.create_window('Woah dude!', 'https://pywebview.flowrl.com')
+webview.start(gui='qt')
 
 listener = mouse.Listener(on_move=on.move, on_click=on.click, on_scroll=on.scroll)
 listener.start()
