6eb61a8604d1d5da5c70b410adcc2d098c1b6112
kangjun1120 <kangjun1120@126.com>
2023-02-13 09:42:59 +0800

    upgrade

--- monitor.py
+++ monitor.py
@@ -4,31 +4,43 @@ begin_time = time.perf_counter()
 event_data = []
 
 
-class Key(object):  # 创建Circle类
-    def __init__(self, r):  # 初始化一个属性r（不要忘记self参数，他是类下面所有方法必须的参数）
-        self.r = r
+class KeyObj(object):
+    def __init__(self):
+        self.move = 1
+        self.click = 2
+        self.scroll = 3
+        self.press = 4
+        self.release = 5
+
+
+Key = KeyObj()
 
 
 def move(x, y):
-    print([(time.perf_counter() - begin_time), (x, y)])
+    event_data.append([(time.perf_counter() - begin_time), [x, y], Key.move])
 
 
 def click(x, y, button, pressed):
-    print(str(time.perf_counter() - begin_time) + ' {0} at {1}'.format(
-        'Pressed' if pressed else 'Released',
-        (x, y)))
+    if pressed:
+        event_data.append([(time.perf_counter() - begin_time), [x, y, 0], Key.click])
+    else:
+        event_data.append([(time.perf_counter() - begin_time), [x, y, 1], Key.click])
 
 
 def scroll(x, y, dx, dy):
-    print(str(time.perf_counter() - begin_time) + ' Scrolled {0} at {1}'.format(
-        'down' if dy < 0 else 'up',
-        (x, y)))
+    if dy < 0:
+        event_data.append([(time.perf_counter() - begin_time), [x, y, 0], Key.scroll])
+    else:
+        event_data.append([(time.perf_counter() - begin_time), [x, y, 1], Key.scroll])
 
 
 def press(key):
     try:
-        print(str(time.perf_counter() - begin_time) + ' al key {0} pressed'.format(
-            key.char))
+        if key.char == 'w':
+            print(event_data)
+            return False
+        else:
+            print(str(time.perf_counter() - begin_time) + ' al key {0} pressed'.format(key.char))
     except AttributeError:
         print(str(time.perf_counter() - begin_time) + ' special key {0} pressed'.format(
             key))
